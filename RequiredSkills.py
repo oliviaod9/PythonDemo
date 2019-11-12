953d667c0a0e4c96045bbcc1481e2a2b69baa50f
Olivia O'Driscoll <44274239+oliviaod9@users.noreply.github.com>
2019-11-12 18:18:29 -0500

    Add files via upload

---
+++ RequiredSkills.py
@@ -0,0 +1,29 @@
+from random import randint
+
+# making a list
+list = ["option1","option2","option3"]
+
+#setting a random number from a list in the range of 1 to 3
+randomNumber = list[randint(0,2)]
+    # remember that computers start counting from 0
+
+#setting varibles
+x = y
+
+# while loop
+while x == y:
+    #put action to do here
+    # player input:
+    x = input("option1, option2, or option3?")
+
+#if statements
+if x = 1:
+    print("yay")
+elif x = 2:
+    print("ok")
+else:
+    print("uh oh")
+
+
+
+
